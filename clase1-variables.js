7cf611bac7416285f0d6767e28500d061265ca91
resparzasoto <rubenesparzasoto95@gmail.com>
2020-06-23 00:13:13 -0600

    Variables

---
+++ clase1-variables.js
@@ -0,0 +1,14 @@
+var nombre = 'Rubén';
+var apellido = 'Esparza';
+var edad = 25;
+
+edad = '25 años';
+
+'Hola ' + nombre + ' ' + apellido;
+'Tengo ' + edad;
+
+var peso = 75;
+
+peso;
+
+peso = '75Kg';
