de163aa2f4484786fda5bf37c5d26fd5aae328fa
resparzasoto <rubenesparzasoto95@gmail.com>
2020-06-23 00:28:15 -0600

    Funciones

---
+++ clase4-funciones.js
@@ -0,0 +1,14 @@
+var nombre = 'Rubén';
+var edad = 25;
+
+function imprimirEdad(n, e) {
+    console.log(`${n} tiene ${e} años`);
+}
+
+imprimirEdad(nombre, edad);
+imprimirEdad('Vicky', 28);
+imprimirEdad('Erick', 24);
+imprimirEdad('Darío', 27);
+imprimirEdad(25, 'Carlos');
+imprimirEdad();
+imprimirEdad('Juan');
