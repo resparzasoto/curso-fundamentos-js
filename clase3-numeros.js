936cf0c279689aaf783fff79842c2b05e50abc8d
resparzasoto <rubenesparzasoto95@gmail.com>
2020-06-23 00:15:50 -0600

    Variables: Números

---
+++ clase3-numeros.js
@@ -0,0 +1,49 @@
+var age = 25;
+
+age = age + 1;
+
+age += 1;
+
+var weight = 75;
+
+weight = weight - 2;
+
+weight -= 2;
+
+var sandwich = 1;
+
+weight = weight + sandwich;
+
+var playFootball = 3;
+
+weight -= playFootball;
+
+var priceOfWine = 200.3;
+
+priceOfWine;
+
+var total = priceOfWine * 3;
+
+total;
+
+var total = priceOfWine * 100 * 3 / 100;
+
+total;
+
+var total = Math.round(priceOfWine * 100 * 3) / 100;
+
+total;
+
+var totalStr = total.toFixed(2);
+
+totalStr;
+
+var total2 = parseFloat(totalStr);
+
+total2;
+
+var pizza = 8;
+var people = 2;
+var numberOfServingsForPerson = pizza / people;
+
+numberOfServingsForPerson;
