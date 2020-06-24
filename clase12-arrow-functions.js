2ada62f1ee2292dbda384dceaed3f1d2cdfc9944
resparzasoto <rubenesparzasoto95@gmail.com>
2020-06-24 07:04:34 -0600

    Arrow functions

---
+++ clase12-arrow-functions.js
@@ -0,0 +1,55 @@
+const MAJOR_OF_AGE = 18;
+
+var sacha = {
+    name: 'Sacha',
+    lastName: 'Lifszyc',
+    age: 28
+};
+
+var juan = {
+    name: 'Juan',
+    lastName: 'Gomez',
+    age: 15
+};
+
+// Anonymous function
+// const isMajorOfAge = function (person) {
+//     return person.age >= MAJOR_OF_AGE;
+// }
+
+// Arrow function
+// const isMajorOfAge = (person) => {
+//     return person.age >= MAJOR_OF_AGE;
+// }
+
+// Arrow function in line
+// const isMajorOfAge = person => person.age >= MAJOR_OF_AGE;
+
+// Arrow function in line with destructured object
+const isMajorOfAge = ({ age }) => age >= MAJOR_OF_AGE;
+
+function printIfPersonIsMajorOfAge(person) {
+    if (isMajorOfAge(person)) {
+        console.log(`${person.name} is major of age!`);
+    }
+    else
+    {
+        console.log(`${person.name} is minor of age!`);
+    }
+}
+
+function allowAccess(person) {
+    if (!isMajorOfAge(person)) {
+        console.log('Access denied');
+    }
+    else
+    {
+        console.log('Allow access');
+    }
+}
+
+printIfPersonIsMajorOfAge(sacha);
+allowAccess(sacha);
+
+printIfPersonIsMajorOfAge(juan);
+allowAccess(juan);
