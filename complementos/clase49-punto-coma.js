71105975726e0de3f534205d00dcf3b4bf660a9a
resparzasoto <rubenesparzasoto95@gmail.com>
2020-06-30 19:06:22 -0600

    ¿Cuándo hace falta poner el punto y coma al final de la línea?

---
+++ complementos/clase49-punto-coma.js
@@ -0,0 +1,13 @@
+console.log('Hola mi nombre es Rubén')
+;[1, 2, 3].forEach(n => console.log(n * 2))
+
+const name = 'Rubén'
+console.log('Hola mi nombre es Rubén');
+`${name} es un desarrollador`
+
+function calculateDouble(n) {
+    return {
+        original: n,
+        double: n * 2
+    }
+}
